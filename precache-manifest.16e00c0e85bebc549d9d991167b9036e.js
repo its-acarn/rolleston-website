b90693bb4e5d440cd6997251a4447d10046c371c
Marc Edees <Marc@Marcs-MacBook-Pro-4.local>
2020-07-07 06:55:46 +0100

    Updates

---
+++ precache-manifest.16e00c0e85bebc549d9d991167b9036e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1c8c356a270982c3aa8b43b12e5d7d",
+    "url": "/rolleston-website/index.html"
+  },
+  {
+    "revision": "874d09f06058e542486e",
+    "url": "/rolleston-website/static/css/main.53425a33.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/rolleston-website/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolleston-website/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874d09f06058e542486e",
+    "url": "/rolleston-website/static/js/main.ee9ca3eb.chunk.js"
+  },
+  {
+    "revision": "7e72d1788260f3e1d7f9",
+    "url": "/rolleston-website/static/js/runtime-main.99cbf4a6.js"
+  }
+]);
